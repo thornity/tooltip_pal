ad749c96b6252135b6b3be71a5ee91191f277d79
dotblank <EliasWoods@gmail.com>
2024-01-21 23:34:33 -0500

    Add canvas transform so behaviour is more universal
    Move functions from resources.
    Add canvas transformations

--- scripts/tpal_draw.gd
+++ scripts/tpal_draw.gd
@@ -49,17 +49,26 @@ func force_inside_viewport(tooltip):
 	if tooltipEndPos.y > viewport.y:
 		tooltip.global_position.y = viewport.y - tooltip.size.y
 
-func generate_tpal(position_from: Node) -> Control:
-	tooltip = panel.instantiate() as Control
-	tooltip.global_position = position_from.global_position + determineShift(direction, directionMargin, tooltip.size, position_from_size(position_from))
+func generate_tpal() -> Control:
+	return panel.instantiate()
 
-	if position_from.has_method("get_screen_position"):
-		print_debug(position_from.get_screen_position())
-
-	if force_in_bounds: 
-		force_inside_viewport(tooltip)
-		
-	return tooltip
+#func generate_tpal(position_from: Node, destinationTransform: Transform2D) -> Control:
+	#
+	#var final_position := Vector2(0,0)
+	#if position_from.position:
+		#var transform := position_from.get_viewport().get_final_transform()
+		#final_position = transform.affine_inverse() * position_from.position
+	#
+	#tooltip = panel.instantiate() as Control
+	#tooltip.position = (final_position * destinationTransform) + determineShift(direction, directionMargin, tooltip.size, position_from_size(position_from))
+#
+	#if position_from.has_method("get_screen_position"):
+		#print_debug(position_from.get_screen_position())
+#
+	#if force_in_bounds: 
+		#force_inside_viewport(tooltip)
+		#
+	#return tooltip
 
 func position_from_size(position_from: Node) -> Vector2:
 	var position_from_size = Vector2(0,0)
